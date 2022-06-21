de3f6d3f73b0ce0d892e5a2b5adfebb0e40840cb
Taron Vardanyan <taron.vardanyan@betconstruct.com>
2022-06-21 13:13:37 +0400

    added auth middleware for apollo

--- src/graphql/mutation/index.js
+++ src/graphql/mutation/index.js
@@ -13,3 +13,30 @@ export const CREATE_LINK_MUTATION = gql`
     }
   }
 `
+
+export const SIGNUP_MUTATION = gql`
+  mutation SignupMutation(
+    $email: String!
+    $password: String!
+    $name: String!
+  ) {
+    signup(
+      email: $email
+      password: $password
+      name: $name
+    ) {
+      token
+    }
+  }
+`;
+
+export const LOGIN_MUTATION = gql`
+  mutation LoginMutation(
+    $email: String!
+    $password: String!
+  ) {
+    login(email: $email, password: $password) {
+      token
+    }
+  }
+`;
