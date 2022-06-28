b50b229b5e40c26466a5f5833a61ef25e03c7f24
Taron Vardanyan <taron.vardanyan@betconstruct.com>
2022-06-28 17:30:10 +0400

    added pagination

--- src/components/App.js
+++ src/components/App.js
@@ -4,25 +4,31 @@ import Header from './Header';
 import Login from './Login';
 import LinkList from './LinkList';
 import Search from './Search';
-import { Route, Routes } from 'react-router-dom';
+import { Navigate, Route, Routes } from 'react-router-dom';
 
-const App = () => {
-  return (
-      <div className="center w85">
+const App = () => (
+    <div className="center w85">
         <Header />
         <div className="ph3 pv1 background-gray">
-          <Routes>
-            <Route path="/" element={<LinkList/>} />
-            <Route
-                path="/create"
-                element={<CreateLink/>}
-            />
-              <Route path="/login" element={<Login/>} />
-              <Route path="/search" element={<Search/>}/>
-          </Routes>
+            <Routes>
+                <Route
+                    path="/"
+                    element={<Navigate replace to="/new/1" />}
+                />
+                <Route
+                    path="/create"
+                    element={<CreateLink/>}
+                />
+                <Route path="/login" element={<Login/>}/>
+                <Route path="/search" element={<Search/>}/>
+                <Route path="/top" element={<LinkList/>} />
+                <Route
+                    path="/new/:page"
+                    element={<LinkList/>}
+                />
+            </Routes>
         </div>
-      </div>
-  );
-};
+    </div>
+);
 
 export default App;
